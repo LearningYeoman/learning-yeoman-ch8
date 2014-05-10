647bcb4d059a50701f41c19aa8b399170d2ad65d
Jonnie Spratley <jonniespratley@me.com>
2014-05-09 20:28:01 -0700

    cleaned up code

--- tasks/learning_yeoman_ch8.js
+++ tasks/learning_yeoman_ch8.js
@@ -5,47 +5,63 @@
  * Copyright (c) 2014 Jonnie Spratley
  * Licensed under the MIT license.
  */
-
 'use strict';
-
 module.exports = function (grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	/**
+	 * Custom Grunt task example.
+	 * @constructor
+	 */
+	function LearningYeomanCh8() {
 
-  grunt.registerMultiTask('learning_yeoman_ch8', 'This is an example plugin.', function () {
+		/**
+		 * I merge task-specific and/or target-specific options with these defaults.
+		 */
+		var options = this.options( {
+			punctuation: '.',
+			separator: ', '
+		} );
 
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+		/**
+		 * I warn on and remove invalid source files (if nonull was set).
+		 * @param filepath
+		 * @returns {boolean}
+		 */
+		var checkFiles = function (filepath) {
+			if (!grunt.file.exists( filepath )) {
+				grunt.log.warn( 'Source file "' + filepath + '" not found.' );
+				return false;
+			} else {
+				return true;
+			}
+		};
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+		/**
+		 * I read the source contents from the specified file.
+		 * @param file
+		 * @returns {*|string}
+		 */
+		var readSource = function (file) {
+			return file.src.filter( checkFiles ).map( function (filepath) {
+				return grunt.file.read( filepath );
+			} ).join( grunt.util.normalizelf( options.separator ) );
+		}
 
-      // Handle options.
-      src += options.punctuation;
+		/**
+		 * I handle reading and writing a file with the passed in options template.
+		 * @param file
+		 */
+		var readWriteFile = function (file) {
+			var src = readSource( file );
+					src += options.punctuation;
 
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
+			grunt.file.write( file.dest, src );
+			grunt.log.writeln( 'File "' + file.dest + '" created.' );
+		};
 
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
-  });
+		// Iterate over all specified file groups.
+		this.files.forEach( readWriteFile );
 
+	};
+	grunt.registerMultiTask( 'learning_yeoman_ch8', 'This is an example plugin.', LearningYeomanCh8 );
 };
